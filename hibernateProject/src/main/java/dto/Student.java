b46d8696ead39f98622db1bca2e78b79e2da92f7
Sooraj Tharayil <sooraj114ts@gmail.com>
2022-12-30 22:02:41 +0530

    Student class modified and completed

--- hibernateProject/src/main/java/dto/Student.java
+++ hibernateProject/src/main/java/dto/Student.java
@@ -1,5 +1,41 @@
 package dto;
 
-public class Student {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Student {
+    
+	@Id
+	private int Student_id;
+	private String Student_name;
+	private String Student_branch;
+	
+	
+	public int getStudent_id() {
+		return Student_id;
+	}
+	public void setStudent_id(int student_id) {
+		Student_id = student_id;
+	}
+	public String getStudent_name() {
+		return Student_name;
+	}
+	public void setStudent_name(String student_name) {
+		Student_name = student_name;
+	}
+	public String getStudent_branch() {
+		return Student_branch;
+	}
+	public void setStudent_branch(String student_branch) {
+		Student_branch = student_branch;
+	}
+	@Override
+	public String toString() {
+		return "Student [Student_id=" + Student_id + ", Student_name=" + Student_name + ", Student_branch="
+				+ Student_branch + "]";
+	}
+	
+	
+	
 }
