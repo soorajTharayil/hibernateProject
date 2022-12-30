383588a45be24892b2239eecca57c3a6d6495225
Sooraj Tharayil <sooraj114ts@gmail.com>
2022-12-30 22:27:41 +0530

    Sample class modified and completed.

--- hibernateProject/src/main/java/dao/Sample.java
+++ hibernateProject/src/main/java/dao/Sample.java
@@ -1,5 +1,71 @@
 package dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import dto.Sample;
+import dto.Student;
+
 public class Sample {
 
+	EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("dev");
+	EntityManager entityManager=entityManagerFactory.createEntityManager();
+	EntityTransaction entityTransaction=entityManager.getTransaction();
+	
+	public void addStudent(int id,String name,String branch)
+    {
+    	entityTransaction.begin();
+    	Student student=new Student();
+    	student.setStudent_id(id);
+    	student.setStudent_name(name);
+    	student.setStudent_branch(branch);
+    	
+    	entityManager.persist(student);
+    	entityTransaction.commit();
+    }
+	
+	public void fetchStudent(int id)
+    {
+    	Student student=entityManager.find(Student.class, id);
+    	System.out.println(student);
+    	
+    }
+	
+	public void removeStudent(int id)
+    {
+    	entityTransaction.begin();
+    	Student student=entityManager.find(Student.class, id);
+    	entityManager.remove(student);
+    	entityTransaction.commit();
+    }
+	
+	public void updateStudent(int id,String name)
+    {   entityTransaction.begin();
+    	Student student=entityManager.find(Student.class, id);
+    	student.setStudent_name(name);
+    	entityManager.merge(student);
+    	entityTransaction.commit();
+    	
+    }
+	
+	 public void fetchAll()
+	    {
+	    	Query query=entityManager.createQuery("select a from Student a");
+	        List<Student> list=	query.getResultList();
+	        for(Student student:list)
+	        System.out.println(student);
+	    }
+	 
+	   public void deleteAll()
+	    {
+	    	Query query=entityManager.createQuery("delete from Student");
+	    	entityTransaction.begin();
+	    	query.executeUpdate();
+	    	entityTransaction.commit();
+	    }
 }
